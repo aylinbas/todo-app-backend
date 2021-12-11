665b20fcfdca145837816b8e48ab9aca321ea62f
aylinbas <ayln.bas.50@hotmail.com>
2021-12-11 21:25:24 +0300

    docker

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-import express from "express";
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  console.log("deneme");
+  res.send("<h1>Hello world</h1>");
+});
 const todos = [
   { id: "0a", title: "deneme1", description: "description1", isDone: false },
   { id: "1b", title: "deeme2", description: "description2", isDone: false },
@@ -18,5 +28,6 @@ app.post("/", async (req, res) => {
 
   res.send({ todos: todos });
 });
+app.listen(8080, () => console.log("listening on port 8080"));
 
-export default app;
+module.exports = app;
