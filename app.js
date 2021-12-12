399d19472dda7911c240ea6e9415cbf2fb6ffaa5
aylinbas <ayln.bas.50@hotmail.com>
2021-12-12 13:18:36 +0300

    ports

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ const client = require("./redis");
 const uniqid = require("uniqid");
 
 var corsOptions = {
-  origin: "http://localhost:3002",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 app.get("/", async (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
-const todos = [];
 
 app.use(express.json());
 app.post("/", async (req, res) => {
@@ -31,13 +30,11 @@ app.post("/", async (req, res) => {
       isDone,
     })
   );
-  client.hgetall("users:123", function (err, obj) {
-    console.dir(obj);
-  });
+
   client.hgetall("todo", function (err, obj) {
     res.send({ todos: obj });
   });
 });
-app.listen(3005, () => console.log("listening on port 8080"));
+app.listen(8080, () => console.log("listening on port 8080"));
 
 module.exports = app;
