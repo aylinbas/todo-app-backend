02237d08a0f8a613727471da60d1878fcca8bd40
aylinbas <ayln.bas.50@hotmail.com>
2021-12-13 21:32:44 +0300

    ..

--- app.js
+++ app.js
@@ -7,63 +7,44 @@ const uniqid = require("uniqid");
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
-// app.use(cors());
+app.use(
+  cors({
+    origin:
+      " https://8080-1e316105-1adc-490c-b678-5d7376cc7965.cs-europe-west4-bhnf.cloudshell.dev/?authuser=0",
+  })
+);
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.get("/", async (req, res) => {
-  client.hgetall("todo", function (err, obj) {
-    res.send({ todos: obj });
-  });
-  // client.flushdb(function (err, succeeded) {
-  //   console.log(succeeded);
-  // });
-});
+// app.get("/", async (req, res) => {
+//   client.hgetall("todo", function (err, obj) {
+//     res.send({ todos: obj });
+//   });
+//   // client.flushdb(function (err, succeeded) {
+//   //   console.log(succeeded);
+//   // });
+// });
 
 app.use(express.json());
-app.post("/", async (req, res) => {
-  const { title, description, isDone } = req.body;
-  if (!title || !description || !isDone) {
-    res.sendStatus(400);
-    return;
-  }
+// app.post("/", async (req, res) => {
+//   const { title, description, isDone } = req.body;
+//   if (!title || !description || !isDone) {
+//     res.sendStatus(400);
+//     return;
+//   }
 
-  client.hset(
-    "todo",
-    uniqid(),
-    JSON.stringify({
-      title,
-      description,
-      isDone,
-    })
-  );
+//   client.hset(
+//     "todo",
+//     uniqid(),
+//     JSON.stringify({
+//       title,
+//       description,
+//       isDone,
+//     })
+//   );
 
-  client.hgetall("todo", function (err, obj) {
-    res.send({ todos: obj });
-  });
-});
+//   client.hgetall("todo", function (err, obj) {
+//     res.send({ todos: obj });
+//   });
+// });
 app.listen(PORT, HOST, () => console.log("listening on port 8080"));
 
 module.exports = app;
