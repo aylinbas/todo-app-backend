484652766122a52e0786eb386e58b2e0ade4b42e
aylinbas <ayln.bas.50@hotmail.com>
2021-12-13 21:03:54 +0300

    ..

--- app.js
+++ app.js
@@ -8,27 +8,14 @@ const PORT = 8080;
 const HOST = "0.0.0.0";
 
 app.use(cors());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // Request methods you wish to allow
-  res.setHeader(
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    "GET,POST,PUT,HEAD,DELETE,OPTIONS"
   );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
+  res.header("Access-Control-Allow-Headers", "content-Type,x-requested-with");
   next();
 });
 
