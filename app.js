64fb351dd2b6262e09715db7c39799fc997f1954
aylinbas <ayln.bas.50@hotmail.com>
2021-12-14 21:43:01 +0300

    cors error

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  res.setHeader("Access-Control-Allow-Credentials", true);
 
   next();
 });
